01731849295abf518aa95654e3d0411818c73521
2001Ujjawal <ujjawalkpa09@gmail.com>
2025-12-18 15:08:01 +0530

    remove demo routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,7 +10,7 @@ $routes->get('/', 'Home::index');
 $routes->group('', ['namespace' => 'App\Controllers'], function ($routes) {
     $routes->get('login', 'AuthController::index');
     $routes->post('admin-login', 'AuthController::authLogin');
-    $routes->post('cookie', 'AuthController::cookie');
+
 });
 
 $routes->group('', ['namespace' => 'App\Controllers'], function ($routes) {
