f7885535d8cdeefd0e35a78ae2f2c8991235f061
2001Ujjawal <ujjawalkpa09@gmail.com>
2025-12-18 11:33:55 +0530

    user controller

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\CommonController;
+
+
+class UserController extends CommonController
+{
+    public function index()
+    {
+        return "hello";
+    }
+}
