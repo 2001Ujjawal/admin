4b35fcfb670b637c2b281af53b7998e01192da8b
2001Ujjawal <ujjawalkpa09@gmail.com>
2025-12-30 10:18:09 +0530

    added uid generate helper function

--- app/Helpers/common_helper_functions.php
+++ app/Helpers/common_helper_functions.php
@@ -16,3 +16,18 @@ if (!function_exists('prt')) {
         return;
     }
 };
+
+
+if (!function_exists('generateUid')) {
+    function generateUid($prefix = null, $length = 8)
+    {
+        $alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+        $id = '';
+        $maxIndex = strlen($alphabet) - 1;
+
+        for ($i = 0; $i < $length; $i++) {
+            $id .= $alphabet[random_int(0, $maxIndex)];
+        }
+        return $prefix . $id;
+    }
+}
